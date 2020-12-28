07556bb2c5ec9f8ceaef9740d5cc7c73ec8a2299
Коледа Наталья Валерьевна <natallia.kaliada@sc.softclub.by>
2020-12-28 17:38:19 +0300

    Remove event listening.

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -10,8 +10,11 @@ export class Excel {
 		const $root = $.create('div', 'excel');
 		this.components = this.components.map(Component => {
 			const $el = $.create('div', Component.className);
-
 			const component = new Component($el);
+			// DEBUG
+			if (component.name) {
+				window['cc' + component.name] = component;
+			}
 			$el.html(component.toHTML());
 			$root.append($el);
 			return component;
--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -13,16 +13,21 @@ export class DomListener {
 		this.listeners.forEach(listener => {
 			const method = getMethodName(listener);
 			if (!this[method]) {
-				const name = this.name || '';
 				throw new Error(`
-				Method ${method} is not implemented in ${name} Component
+				Method ${method} is not implemented in ${this.name} Component
 				`);
 			}
-			this.$root.on(listener, this[method].bind(this) );
+			this[method] = this[method].bind(this);
+			this.$root.on(listener, this[method]);
 		});
 	}
 
-	removeDOMListeners() {}
+	removeDOMListeners() {
+		this.listeners.forEach(listener => {
+			const method = getMethodName(listener);
+			this.$root.off(listener, this[method]);
+		});
+	}
 }
 
 function getMethodName(method) {
