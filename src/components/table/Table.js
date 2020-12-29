5b20cee1213105c50d54873ee68892f7fb212702
Коледа Наталья Валерьевна <natallia.kaliada@sc.softclub.by>
2020-12-29 16:32:49 +0300

    Creating resize element.

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -3,8 +3,18 @@ import { createTable } from './table.template';
 
 export class Table extends ExcelComponent {
 	static className = 'excel__table';
+	constructor($root) {
+		super($root, {
+			name: 'Table',
+			listeners: ['mousedown']
+		});
+	}
 
 	toHTML() {
 		return createTable(20);
+	}
+
+	onMousedown(e) {
+		console.log('down', e.target);
 	}
 }
